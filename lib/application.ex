d0c969dbf037ea653da77da6dbab116e437948da
Divya Saxena <54782918+divyasaxenaa@users.noreply.github.com>
2020-10-17 21:28:41 -0400

    Fixing broken code (#3)

--- lib/application.ex
+++ lib/application.ex
@@ -2,27 +2,14 @@ defmodule VendingMachine.Application do
   @moduledoc false
   use Application
 
-  alias VendingMachine.{ItemService, Stash, VendingServer}
-
   def start(_type, _args) do
 
     children = [
-      {Stash, ItemService.load_vending_machine()},
-      {VendingServer, []}
+      {VendingMachine.Stash, VendingMachine.ItemService.load_vending_machine()},
+      {VendingMachine.Server, []}
     ]
 
     sup_opts = [strategy: :rest_for_one, name: VendingMachine.Supervisor]
     Supervisor.start_link(children, sup_opts)
   end
-
-  # def start(_type, _args) do
-  #   opts = []
-  #   # {ServerModule, start_link_arg}
-  #   children = [
-  #     {Server, opts}
-  #   ]
-
-  #   opts = [strategy: :one_for_one, name: VendingMachine.Supervisor]
-  #   Supervisor.start_link(children, opts)
-  # end
 end
--- lib/vending_server.ex
+++ lib/vending_server.ex
@@ -1,55 +1,55 @@
-defmodule VendingMachine.VendingServer do
-  use GenServer
-  alias VendingMachine.{VendingServer, ItemController, PaymentController, Stash}
+# defmodule VendingMachine.VendingServer do
+#   use GenServer
+#   alias VendingMachine.{VendingServer, ItemController, PaymentController, Stash}
 
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, %{})
-  end
+#   def start_link(_) do
+#     GenServer.start_link(__MODULE__, %{})
+#   end
 
-  @impl true
-  def init(_state) do
-    Process.flag(:trap_exit, true)
-    initial_vending_machine_state = Stash.get()
-    {:ok, initial_vending_machine_state}
-  end
+#   @impl true
+#   def init(_state) do
+#     Process.flag(:trap_exit, true)
+#     initial_vending_machine_state = Stash.get()
+#     {:ok, initial_vending_machine_state}
+#   end
 
-  # @impl true
-  # def handle_cast(:display_item, vending_machine_state) do
-  #   {:noreply, ItemController.display_item(vending_machine_state)}
-  # end
+#   # @impl true
+#   # def handle_cast(:display_item, vending_machine_state) do
+#   #   {:noreply, ItemController.display_item(vending_machine_state)}
+#   # end
 
-  def handle_call({:verify_key, item_key}, _from, state) do
-    {is_item_key_available, new_state} = ItemController.verify_key(item_key, state)
-    {:reply, is_item_key_available, new_state}
-  end
+#   def handle_call({:verify_key, item_key}, _from, state) do
+#     {is_item_key_available, new_state} = ItemController.verify_key(item_key, state)
+#     {:reply, is_item_key_available, new_state}
+#   end
 
-  def handle_call({:verify_quantity, item_quantity}, _from, state) do
+#   def handle_call({:verify_quantity, item_quantity}, _from, state) do
 
-    {is_item_available, new_state} = ItemController.verify_item_availability(item_quantity, item_key, state )
-    {:reply, is_item_available, new_state}
-  end
+#     {is_item_available, new_state} = ItemController.verify_item_availability(item_quantity, item_key, state )
+#     {:reply, is_item_available, new_state}
+#   end
 
-  def handle_call(:accept_item_key, _from, vending_machine_state) do
-    IO.inspect("Gets to VendingMachine.Server.handle_cast_item!!!!!!!!!!!!!!!!!!!")
-    {display, vending_machine_state} = ItemController.get_key_from_user
-    {:reply, display, new_vm}
-  end
+#   def handle_call(:accept_item_key, _from, vending_machine_state) do
+#     IO.inspect("Gets to VendingMachine.Server.handle_cast_item!!!!!!!!!!!!!!!!!!!")
+#     {display, vending_machine_state} = ItemController.get_key_from_user
+#     {:reply, display, new_vm}
+#   end
 
-  @impl true
-  def handle_cast(:display, state) do
-    {:noreply, ItemController.load_vending_machine(state)}
-  end
+#   @impl true
+#   def handle_cast(:display, state) do
+#     {:noreply, ItemController.load_vending_machine(state)}
+#   end
 
 
 
-  @impl true
-  def handle_info(_msg, state) do
-    {:stop, :normal, state}
-  end
+#   @impl true
+#   def handle_info(_msg, state) do
+#     {:stop, :normal, state}
+#   end
 
-  @impl true
-  def terminate(_reason, state) do
-    Stash.update(state)
-  end
+#   @impl true
+#   def terminate(_reason, state) do
+#     Stash.update(state)
+#   end
 
-end
+# end
