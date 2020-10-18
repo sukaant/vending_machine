1e8f15c7eb15049a1f15722a06ab8caa21441eb4
divyasaxenaa <divya.it.saxena@gmail.com>
2020-10-18 01:57:02 -0400

    Adding Neo4j Database Configuration and refactored project structure

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :bolt_sips, Bolt,
+  hostname: 'localhost',
+  basic_auth: [username: "neo4j", password: "puzzle-child-iris-saddle-gelatin-5416"],
+  port: 7687,
+  pool_size: 5,
+  max_overflow: 1
